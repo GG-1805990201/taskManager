7f238f4c4bca32b04b167bc4c3514c2c3db50bd3
GG-1805990201 <70736703+GG-1805990201@users.noreply.github.com>
2023-03-06 06:36:20 +0530

    Increased character limit for title column in Task and AuditTasks Table

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ USE taskmanager;
 
 CREATE TABLE IF NOT EXISTS Tasks (
 taskid INT PRIMARY KEY auto_increment,
-title VARCHAR(20),
+title VARCHAR(200),
 eta VARCHAR(20),
 status VARCHAR(20),
 created_date VARCHAR(20)
@@ -11,7 +11,7 @@ created_date VARCHAR(20)
 CREATE TABLE IF NOT EXISTS AuditTasks (
 audit_log_id INT PRIMARY KEY auto_increment,
 taskid INT,
-title VARCHAR(20),
+title VARCHAR(200),
 updated_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
 updated_fields VARCHAR(200),
 old_values VARCHAR(200),
